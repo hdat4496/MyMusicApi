6369c09d572123cbe49a3b5114696a4454b69a00
toanhda <toanhda@vng.com.vn>
2018-11-17 19:06:33 +0700

    test build model

--- api/controllers/track.js
+++ api/controllers/track.js
@@ -5,9 +5,56 @@ const { generateToken, checkToken } = require('../helpers/token');
 // const { runData } = require('../helpers/data.js');
 const crypto = require('crypto');
 var arff = require('node-arff');
+var weka = require('/home/lap12526-local/MyMusicApi/node_modules/node-weka/lib/weka-lib.js');
+
+
+// var weka = require("node-weka")
 module.exports = {
-    getTrack: getTrack
+    getTrack: getTrack_2
 };
+
+
+function getTrack_2(req, res) {
+
+        arff.load('/home/lap12526-local/MyMusicApi/api/public/arff/abc.arff', function(err, data) {
+            if (err) {
+              return console.error(err);
+            }
+            // console.log(data)
+            var options = {
+                'classifier': 'weka.classifiers.bayes.NaiveBayes',
+                'params'    : ''
+              };
+              var options = {
+                'classifier': 'weka.classifiers.bayes.NaiveBayes',
+                // 'classifier': 'weka.classifiers.functions.SMO',
+                'params'    : ''
+              };
+               
+              var testData = {
+                outlook    : 'sunny',
+                windy      : 'TRUE',
+                temperature: 30,
+                humidity   : 2,
+                play       : 'no' // last is class attribute
+              };
+               
+              weka.classify(data, testData, options, function (err, result) {
+
+
+                console.log(err); //{ predicted: 'yes', prediction: '1' }
+                console.log(result); //{ predicted: 'yes', prediction: '1' }
+                
+              });
+
+               
+          });
+
+
+    
+}
+
+
 
 
 function getTrack(req, res) {
@@ -38,13 +85,16 @@ function getTrack(req, res) {
                                         if (!err) { 
                                             var num_of_listen = value;
                                             
-                                            arff.load('/home/lap12526-local/MyMusicApi/api/api/controllers/arff/occ.arff', function(err, data) {
+                                            arff.load('/home/lap12526-local/MyMusicApi/api/public/arff/training.arff', function(err, data) {
                                                 if (err) {
                                                   return console.error(err);
                                                 }
-                                                console.log('1111111111111111111');
                                                 console.log(data)
-                                                // var oldest = data.max('age');
+                                                var options = {
+                                                    'classifier': 'weka.classifiers.bayes.NaiveBayes',
+                                                    // 'classifier': 'weka.classifiers.functions.SMO',
+                                                    'params'    : ''
+                                                  };
 
                                                
                                                 // normalize the data (scale all numeric values so that they are between 0 and 1)
@@ -52,26 +102,7 @@ function getTrack(req, res) {
                                                
                                                 // randomly sort the data
                                                 // data.randomize();
-                                              })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                                              });
 
                                         }
                                     });
@@ -97,5 +128,10 @@ function getTrack(req, res) {
             res.json({ status: 404, message: '404 Not found' });
         }
     });
+
+
+
+
+
 
 }
